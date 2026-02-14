5bd3482a30b45d155380e0dd3f8b53b1848427d0
Imacqwerty <imacqwertygames@gmail.com>
2026-02-14 20:16:44 +0800

    sdsd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ const homeref = useRef(null);
 
     {/*HERO 1 */}
     <section className={style.homehero1} > 
-      <video autoPlay loop muted playsInline preload="auto"> <source src="/Invent/1.mp4" type="video/mp4" /></video>
+      <video autoPlay loop muted playsInline preload="auto"> <source src="/Invent/11.mp4" type="video/mp4" /></video>
         <div className={style.hero1intro1}>
           <h1><strong>Welcome to Invent</strong></h1>
           <p>Created with ReactJS Framework</p>
